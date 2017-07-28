0610ce9599bc509ef77b1cb02dfec61176119d43
abhisaharan <abhi.saharan@gmail.com>
2017-07-27 22:38:55 -0700

    print only the words that have no “e” and com- pute the percentage of the words in the list have no “e.”

---
+++ Analysis.py
@@ -0,0 +1,14 @@
+fin = open("words.txt", "r")
+ctr = 0
+w_ctr = 0
+for line in fin :
+    word = line.strip()
+    w_ctr += 1
+    if not ( 'e' in word or 'E' in word ) :
+#        print "%s does not contain the letter 'e'" % word  # This takes too long, but is useful for debugging
+        ctr += 1
+print ("There are %d words that do not contain the letter 'e' out of %d words" % (
+    ctr, w_ctr ))
+print ("That is %.2f %%" % (( 100.0 * float(ctr)/float(w_ctr) ),))
+
+fin.close()
--- Open_file.py
+++ Open_file.py
@@ -1,9 +1,15 @@
 fin = open("words.txt", "r")
-ctr = 0
-w_ctr = 0
-for line in fin :
-    word = line.strip()
-    w_ctr += 1
-    if not ( 'e' in word or 'E' in word ) :
-#        print "%s does not contain the letter 'e'" % word  # This takes too long, but is useful for debugging
-        ctr += 1
+count_e = 0
+total = 0
+for word in fin:
+    total+=1
+    for letter in word:
+        if letter == 'e' or letter == "E":
+            count_e+=1
+            break
+
+print("total: {}".format(total))
+print("total e: {}".format(count_e))
+without_e = total-count_e
+print("words without e: {}".format(without_e))
+print(100*(without_e/total))
