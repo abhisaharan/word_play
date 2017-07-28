1214afa1be34220c679f51877ae539c91a9df59d
abhisaharan <abhi.saharan@gmail.com>
2017-07-27 21:40:16 -0700

    finding "E or e"

--- Open_file.py
+++ Open_file.py
@@ -7,6 +7,3 @@ for line in fin :
     if not ( 'e' in word or 'E' in word ) :
 #        print "%s does not contain the letter 'e'" % word  # This takes too long, but is useful for debugging
         ctr += 1
-print ("There are {} words that do not contain the letter 'e' out of {} words".format(ctr, w_ctr))
-print ("That is {}.2f%%".format(( 100.0 * float(ctr)/float(w_ctr) ),))
-fin.close()
